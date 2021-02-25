93fbd6b199326225ed10290de59dd2fdb18761cd
sonny8 <51467994+sonny8@users.noreply.github.com>
2021-02-25 15:04:10 -0700

    Adding python file-progratoolsmain

---
+++ Programtools20.py
@@ -0,0 +1,10 @@
+import time
+greeting = ("Hey from Enersom!")
+
+def ptgreeting():
+    print(greeting)
+
+class div():
+
+    def projintro(projectname):
+        print("Starting... " + projectname)
