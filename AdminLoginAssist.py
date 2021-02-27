3ad4728c36da9a1f272d6b68bb90462c0a10a12f
EmersonLiepe <63976937+EmersonLiepe@users.noreply.github.com>
2021-02-26 17:30:40 -0700

    Add login function

--- AdminLoginAssist.py
+++ AdminLoginAssist.py
@@ -1,1 +1,17 @@
+import time
 
+def login(password, username):
+    print("1: Username")
+    print("2: Password")
+    usernameuser = input ("")
+    passworduser = input ("")
+
+    if usernameuser == username and passworduser == password:
+        pass
+
+    else:
+        quit()
+
+
+    
+    
